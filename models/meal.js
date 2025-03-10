0972c09494f59b217470262cfb3b17d34b410c8e
RConradGe <rcgadduang@gmail.com>
2025-03-09 21:12:44 -0700

    created a meal schema

---
+++ models/meal.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const mealSchema = new mongoose.Schema({
+  mealNumber: Number,
+  protein: Number,
+  carbs: Number,
+  fats: Number,
+  containsFiber: Boolean,
+  fiberContent: Number,
+
+});
+
+const Meal = mongoose.model("Meal", mealSchema);
